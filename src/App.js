e95123a0e5bd7781ee7ae3d0bbac407268b17695
Sergey <sergsmile29@gmail.com>
2023-10-18 15:20:01 +0300

    vugar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { Header } from './components/Header';
+import { Cart } from './components/Cart'
+import { Header } from './components/Header'
+import data from './assets/article.json'
 
 function App() {
+	console.log(data)
+
 	return (
 		<div>
 			<Header />
+			<Cart data={data} />
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
