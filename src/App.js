428ba0b1282295bd2913c0883f8051d43e88e710
Sergey <sergsmile29@gmail.com>
2023-10-18 16:04:35 +0300

    stat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Cart } from './components/Cart'
 import { Header } from './components/Header'
 import data from './assets/article.json'
+import { Statistics } from './components/Statistics'
+import dataStat from './assets/data.json'
 
 function App() {
 	console.log(data)
@@ -9,6 +11,7 @@ function App() {
 		<div>
 			<Header />
 			<Cart data={data} />
+			<Statistics dataStat={dataStat} title='Info' />
 		</div>
 	)
 }
---
+++ src/components/StatCart.jsx
@@ -0,0 +1,6 @@
+export const StatCart = ({ total, title }) => (
+	<li>
+		<h2>{total}</h2>
+		<p>{title}</p>
+	</li>
+)
