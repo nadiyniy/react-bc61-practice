b8bcc497cc6186cea3ecb0051a2882f0fc0fd8d0
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-23 15:46:36 +0300

    updated form on submit

--- src/components/AddEmployee/AddEmployee.jsx
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -1,39 +1,54 @@
-import React from 'react'
+import React from "react";
 
 export class AddEmployee extends React.Component {
-	state = {
-		name: '',
-		salary: '',
-		position: '',
-	}
+  state = {
+    name: "",
+    salary: "",
+    position: "",
+  };
 
-	handleChangeInput = ({ target }) => {
-		const { name, value } = target
+  handleOnSubmit = (e) => {
+    const { name } = this.state;
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    this.props.addUser(this.state);
+    this.setState({
+      name: "",
+      salary: "",
+      position: "",
+    });
+  };
 
-		this.setState({ [name]: value })
-	}
-	handleDepartmentChange = e => {
-		this.setState({ position: e.target.value })
-	}
-	handleOnClick = () => {
-		this.props.addUser(this.state)
-	}
+  handleChangeInput = ({ target }) => {
+    const { name, value } = target;
 
-	render() {
-		const { name, salary } = this.state
-		// dev, qa, manager, hr
-		return (
-			<div>
-				<input name='name' value={name} onChange={this.handleChangeInput} />
-				<input name='salary' value={salary} onChange={this.handleChangeInput} />
-				<select name='department' value={this.state.position} onChange={this.handleDepartmentChange}>
-					<option value='dev'>Dev</option>
-					<option value='qa'>Qa</option>
-					<option value='manager'>Manager</option>
-					<option value='hr'>Hr</option>
-				</select>
-				<button onClick={this.handleOnClick}>Add New Employee</button>
-			</div>
-		)
-	}
+    this.setState({ [name]: value });
+  };
+  handleDepartmentChange = (e) => {
+    this.setState({ position: e.target.value });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    // dev, qa, manager, hr
+    return (
+      <form action="" onSubmit={this.handleOnSubmit}>
+        <input name="name" value={name} onChange={this.handleChangeInput} />
+        <input name="salary" value={salary} onChange={this.handleChangeInput} />
+        <select
+          name="department"
+          value={this.state.position}
+          onChange={this.handleDepartmentChange}
+        >
+          <option value="dev">Dev</option>
+          <option value="qa">Qa</option>
+          <option value="manager">Manager</option>
+          <option value="hr">Hr</option>
+        </select>
+        <button>Add New Employee</button>
+      </form>
+    );
+  }
 }
