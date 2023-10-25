01e2b0b0b8a86cebcd6690017e103dd85fa07d10
Sergey <sergsmile29@gmail.com>
2023-10-25 15:24:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { fetchProducts } from './servises/apiJs';
-import { ProductItem } from './components/ProductItem';
+import React from 'react'
+import { fetchProducts } from './servises/apiJs'
+import { ProductItem } from './components/ProductItem'
 //plan
 // https://dummyjson.com/products - api
 //https://dummyjson.com/products/search?q=phone - search api
 
 // 1. Створити апі і зробити запит за продуктами - [] - Mariia
-// 2. Додати верстку для відображення продуктів -
-// 3. Зробити підгрузку
-// 4. Додати пошук
-// 5. Вивести в модальне вікно зображення
+// 2. Додати верстку для відображення продуктів -[] - Vugar
+// 3. Зробити підгрузку - [] - Vlad
+// 4. Додати пошук -
+// 5. Вивести в модальне вікно зображення -
 // * Додати лоадери, нотіфікації
 
 class App extends React.Component {
@@ -19,51 +19,51 @@ class App extends React.Component {
 		error: null,
 		query: '',
 		skip: 0,
-	};
+	}
 	async componentDidMount() {
-		this.setState({ loading: true });
+		this.setState({ loading: true })
 		try {
-			const { products } = await fetchProducts();
-			this.setState({ products });
-			console.log(products);
+			const { products } = await fetchProducts()
+			this.setState({ products })
+			console.log(products)
 		} catch (err) {
-			console.log(err);
+			console.log(err)
 		} finally {
-			this.setState({ loading: false });
+			this.setState({ loading: false })
 		}
 	}
 	async componentDidUpdate(prevProps, prevState) {
 		if (this.state.skip !== prevState.skip) {
-			this.setState({ loading: true });
+			this.setState({ loading: true })
 
 			try {
-				const { products } = await fetchProducts({ skip: this.state.skip });
-				this.setState((prev) => ({ products: [...prev.products, ...products] }));
+				const { products } = await fetchProducts({ skip: this.state.skip })
+				this.setState(prev => ({ products: [...prev.products, ...products] }))
 			} catch (error) {
-				console.log(error);
+				console.log(error)
 			} finally {
-				this.setState({ loading: false });
+				this.setState({ loading: false })
 			}
 		}
 	}
 
 	handleLoadMore = () => {
-		this.setState((prev) => ({ skip: prev.skip + 4 }));
-	};
+		this.setState(prev => ({ skip: prev.skip + 4 }))
+	}
 
 	render() {
-		const { products } = this.state;
+		const { products } = this.state
 		return (
 			<div>
 				<h1>Product list</h1>
 				<ul>
-					{products.map((item) => (
+					{products.map(item => (
 						<ProductItem product={item} key={item.id} />
 					))}
 				</ul>
 				<button onClick={this.handleLoadMore}>load more</button>
 			</div>
-		);
+		)
 	}
 }
-export default App;
+export default App
