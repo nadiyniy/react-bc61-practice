3ba831a70e180780f79583a21dd54e7c5a835722
Sergey <sergsmile29@gmail.com>
2023-10-30 23:34:04 +0200

    init route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { ContextProvider } from "./context/ContextProvider";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <ContextProvider>
-    <App />
-    <ToastContainer autoClose={1000} />
-  </ContextProvider>
-);
+	<>
+		<App />
+		<ToastContainer autoClose={1000} />
+	</>
+)
