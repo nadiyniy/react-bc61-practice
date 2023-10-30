db24bd4b20c119f9530d09ab209a3728481bc5a4
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-30 15:40:45 +0200

    refactoring at Reducer

--- src/App.js
+++ src/App.js
@@ -1,79 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { Employees } from "./components/Employees/Employees";
-import { AddEmployee } from "./components/AddEmployee/AddEmployee";
-import { SearchUser } from "./components/SearchUser/SearchUser";
-import { Filter } from "./components/Filter/Filter";
-import { toast } from "react-toastify";
-import { useContext } from "react";
-import { UserContext } from "./context/ContextProvider";
+import React, { useEffect, useReducer, useState } from 'react';
+import { Employees } from './components/Employees/Employees';
+import { AddEmployee } from './components/AddEmployee/AddEmployee';
+import { SearchUser } from './components/SearchUser/SearchUser';
+import { Filter } from './components/Filter/Filter';
+import { toast } from 'react-toastify';
+import { useContext } from 'react';
+import { UserContext } from './context/ContextProvider';
+import { employeesReducer, initialState } from './store/reducer';
 //PLAN
 // - Додати контекст для юзера
 // - Використати React Hook Form для форми юзера
 // - Зробити рефакторінг на useReducer
 
 const App = () => {
-  const [employees, setEmployees] = useState([]);
-  const [filter, setFilter] = useState("");
-  const [department, setDepartment] = useState("");
-  const { login, logout, user, isLogin } = useContext(UserContext);
-  useEffect(() => {
-    const savedData = JSON.parse(window.localStorage.getItem("employees"));
-    if (savedData?.length) {
-      setEmployees(savedData);
-    }
-  }, []);
+	const [state, dispatch] = useReducer(employeesReducer, initialState);
+	const { employees, filter, department } = state;
 
-  useEffect(() => {
-    window.localStorage.setItem("employees", JSON.stringify(employees));
-  }, [employees]);
+	// const [employees, setEmployees] = useState([]);
+	// const [filter, setFilter] = useState('');
+	// const [department, setDepartment] = useState('');
 
-  const handleDeleteUser = (id) => {
-    setEmployees((prev) => prev.filter((user) => user.id !== id));
-    toast.success(`Congratulations, you have deleted user`);
-  };
-  const handleAddNewEmployee = (newData) => {
-    const existingName = employees.find((item) => item.name === newData.name);
-    if (existingName) {
-      alert(`${newData.name} alredy exsists`);
-    } else {
-      const newUser = { id: crypto.randomUUID(), ...newData };
-      setEmployees((prev) => [...prev, newUser]);
-    }
-  };
-  const handleChangeFilter = (e) => {
-    setFilter(e.target.value);
-  };
-  const getFilterData = () => {
-    return employees
-      .filter((user) => user.name.toLowerCase().includes(filter.toLowerCase()))
-      .filter((user) =>
-        department === "all"
-          ? user
-          : user.position.toLowerCase().includes(department.toLowerCase())
-      );
-  };
-  const handleChangeDepartment = (department) => {
-    setDepartment(department);
-    toast.info(`Congratulations, you have changed to ${department}`);
-  };
+	const { login, logout, user, isLogin } = useContext(UserContext);
 
-  const btns = [...new Set(employees.map((user) => user.position))];
-  return (
-    <div>
-      <AddEmployee addUser={handleAddNewEmployee} />
-      <SearchUser setFilter={handleChangeFilter} />
-      {isLogin ? (
-        <button onClick={logout}>exit</button>
-      ) : (
-        <button onClick={() => login("Andre")}>enter</button>
-      )}
+	useEffect(() => {
+		const savedData = JSON.parse(window.localStorage.getItem('employees'));
+		if (savedData?.length) {
+			dispatch({ type: 'loadFromLS', payload: savedData });
+			// setEmployees(savedData);
+		}
+	}, []);
 
-      {user && <h1>Welcome {user}</h1>}
-      <br />
-      <Filter onChangeDepartment={handleChangeDepartment} btns={btns} />
-      <Employees deleteUser={handleDeleteUser} dataEmployee={getFilterData()} />
-    </div>
-  );
+	useEffect(() => {
+		window.localStorage.setItem('employees', JSON.stringify(employees));
+	}, [employees]);
+
+	const handleDeleteUser = (id) => {
+		dispatch({ type: 'deleteUser', payload: id });
+		// setEmployees((prev) => prev.filter((user) => user.id !== id));
+		toast.success(`Congratulations, you have deleted user`);
+	};
+
+	const handleAddNewEmployee = (newData) => {
+		const existingName = employees.find((item) => item.name === newData.name);
+		if (existingName) {
+			alert(`${newData.name} alredy exsists`);
+		} else {
+			const newUser = { id: crypto.randomUUID(), ...newData };
+			dispatch({ type: 'addUser', payload: newUser });
+			// setEmployees((prev) => [...prev, newUser]);
+		}
+	};
+	const handleChangeFilter = (e) => {
+		dispatch({ type: 'changeFilter', payload: e.target.value });
+		// setFilter(e.target.value);
+	};
+	const getFilterData = () => {
+		return employees
+			.filter((user) => user.name.toLowerCase().includes(filter.toLowerCase()))
+			.filter((user) => (department === 'all' ? user : user.position.toLowerCase().includes(department.toLowerCase())));
+	};
+	const handleChangeDepartment = (department) => {
+		dispatch({ type: 'changeDepartment', payload: department });
+		// setDepartment(department);
+		toast.info(`Congratulations, you have changed to ${department}`);
+	};
+
+	const btns = [...new Set(employees.map((user) => user.position))];
+	return (
+		<div>
+			<AddEmployee addUser={handleAddNewEmployee} />
+			<SearchUser setFilter={handleChangeFilter} />
+			{isLogin ? <button onClick={logout}>exit</button> : <button onClick={() => login('Andre')}>enter</button>}
+
+			{user && <h1>Welcome {user}</h1>}
+			<br />
+			<Filter onChangeDepartment={handleChangeDepartment} btns={btns} />
+			<Employees deleteUser={handleDeleteUser} dataEmployee={getFilterData()} />
+		</div>
+	);
 };
 
 export default App;
