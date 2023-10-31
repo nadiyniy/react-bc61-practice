dbfa7b8004841784adfec56b6a7f69cdae788c80
Sergey <sergsmile29@gmail.com>
2023-10-31 23:03:28 +0200

    pr9 init

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./page/Home";
-import About from "./page/About";
-import NotFound from "./page/NotFound";
-import { Layout } from "./components/Layout";
-import Company from "./components/Company";
-import Mission from "./components/Mission";
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from './page/Home'
+import About from './page/About'
+import NotFound from './page/NotFound'
+import { Layout } from './components/Layout'
+import Company from './components/Company'
+import Mission from './components/Mission'
 
 // PLAN
-// - Підключити роутінг, додати BrowserRouter, Routes, Route
-// - Створити роут для 404
-// - Додати Navbar з Navlink
-// - Зробити вкладені маршрути
+// - Підключити роутінг, додати BrowserRouter, Routes, Route - Vlad
+// - Створити роут для 404 - Vugar
+// - Додати Navbar з Navlink - Olena
+// - Зробити вкладені маршрути - Maria
+// - Зробити запит за даними і відмалювати розмітку
+// - Зробити кнопку назад useRef
+// - Використати useSearchParams для пошуку
+// - Додати lazy
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />}>
-          <Route path="company" element={<Company />} />
-          <Route path="mission" element={<Mission />} />
-        </Route>
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='about' element={<About />}>
+					<Route path='company' element={<Company />} />
+					<Route path='mission' element={<Mission />} />
+				</Route>
 
-        <Route path="me" element={<Navigate to="/" />} />
-        <Route path="login" element={<h1>login</h1>} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
+				<Route path='me' element={<Navigate to='/' />} />
+				<Route path='login' element={<h1>login</h1>} />
+			</Route>
+			<Route path='*' element={<NotFound />} />
+		</Routes>
+	)
+}
 
-export default App;
+export default App
