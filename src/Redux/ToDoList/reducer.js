d1b69d75915cbea27b16e8bde38d8de145c3cf32
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-11-03 14:35:05 +0200

    create state

---
+++ src/Redux/ToDoList/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  todos: [
+    { id: 1, todo: "Text", completed: false },
+    { id: 2, todo: "Text", completed: false },
+    { id: 3, todo: "Text", completed: false },
+  ],
+};
+
+export const toDoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "DELETE": {
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
