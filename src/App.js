a285105ead6b664c51e511277dccca6e93a4e2eb
Sergey <sergsmile29@gmail.com>
2023-11-10 01:43:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, { useEffect } from "react";
-import { Home } from "./pages/Home";
-import { useDispatch } from "react-redux";
-import { fetchPostsThunk } from "./redux/posts/operations";
-import { Route, Routes } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import LoginForm from "./components/LoginForm";
-import { Login } from "./pages/Login";
-import { NotFound } from "./pages/NotFound";
-import { AddPost } from "./pages/AddPost";
+import React, { useEffect } from 'react'
+import { Home } from './pages/Home'
+import { useDispatch } from 'react-redux'
+import { fetchPostsThunk } from './redux/posts/operations'
+import { Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import LoginForm from './components/LoginForm'
+import { Login } from './pages/Login'
+import { NotFound } from './pages/NotFound'
+import { AddPost } from './pages/AddPost'
 
-// Створити редакс папки , додати слайс постів, додати стор, підключити редакс
-// Додати селектори, витягнути дані з через редакс, додати operations, підключити mockapi
-// Додати можливість видалення, редагування поста
-// Додати роути для створення / видалення поста
-// Додати час, коли був створений пост
-// Додати користувача, щоб можна було залишати коментарі
+// Створити редакс папки , додати слайс постів, додати стор, підключити редакс - [x] -
+// Додати селектори, витягнути дані з через редакс, додати operations, підключити mockapi - [x] -
+// Додати можливість видалення, редагування поста - [x] -
+// Додати роути для створення / видалення поста - [x] -
+// Додати час, коли був створений пост - [x] -
+// Додати користувача, щоб можна було залишати коментарі - [x] -
+// Додати логін користувача, переробити слайс, написати операції
+// Логаут
+// Рефреш
 // Організувати перехід на readMore. Дивитись коментарі для поста
 //
 export const App = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(fetchPostsThunk());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchPostsThunk())
+	}, [dispatch])
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="addPost" element={<AddPost />} />
-          <Route path="login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </>
-  );
-};
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path='addPost' element={<AddPost />} />
+					<Route path='login' element={<Login />} />
+				</Route>
+				<Route path='*' element={<NotFound />}></Route>
+			</Routes>
+		</>
+	)
+}
