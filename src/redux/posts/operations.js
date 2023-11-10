430f41819e51a44a1b167f4e7c3a1a28af628cbb
Sergey <sergsmile29@gmail.com>
2023-11-10 14:15:40 +0200

    update

--- src/redux/posts/operations.js
+++ src/redux/posts/operations.js
@@ -1,40 +1,31 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-axios.defaults.baseURL = "https://654a2082e182221f8d528c04.mo1ckapi.io/";
+axios.defaults.baseURL = 'https://654a2082e182221f8d528c04.mockapi.io/'
 
-export const fetchPostsThunk = createAsyncThunk(
-  "fetchPosts",
-  async (_, thunkApi) => {
-    try {
-      const { data } = await axios.get("posts");
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
-    }
-  }
-);
+export const fetchPostsThunk = createAsyncThunk('fetchPosts', async (_, thunkApi) => {
+	try {
+		const { data } = await axios.get('posts')
+		return data
+	} catch (error) {
+		return thunkApi.rejectWithValue(error.message)
+	}
+})
 
-export const deletePostsThunk = createAsyncThunk(
-  "deletePost",
-  async (id, thunkApi) => {
-    try {
-      const { data } = await axios.delete(`posts/${id}`);
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
-    }
-  }
-);
+export const deletePostsThunk = createAsyncThunk('deletePost', async (id, thunkApi) => {
+	try {
+		const { data } = await axios.delete(`posts/${id}`)
+		return data
+	} catch (error) {
+		return thunkApi.rejectWithValue(error.message)
+	}
+})
 
-export const addPostThunk = createAsyncThunk(
-  "addPost",
-  async (body, thunkApi) => {
-    try {
-      const { data } = await axios.post("posts", body);
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
-    }
-  }
-);
+export const addPostThunk = createAsyncThunk('addPost', async (body, thunkApi) => {
+	try {
+		const { data } = await axios.post('posts', body)
+		return data
+	} catch (error) {
+		return thunkApi.rejectWithValue(error.message)
+	}
+})
